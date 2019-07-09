73c3382f1651342450f29c1fa59b42768abb3a86
Heiko <fenghenyu@163.com>
2019-07-09 08:59:46 +0800

    商铺列表数据获取

---
+++ xcx_02/pages/shoplist/shoplist.js
@@ -0,0 +1,86 @@
+// pages/shoplist/shoplist.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    opts:{},
+    //商铺列表数据，默认为空
+    shopList:[],
+    page:1,
+    pageSize:10
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.setData({
+      opts:options
+    })
+
+    //获取商铺列表数据
+    this.getShopList()
+  },
+
+  getShopList : function(){
+    wx.request({
+      url: 'https://www.liulongbin.top:8081/categories/'+this.data.opts.id+'/shops?_page='+this.data.page+'&_limit='+this.data.pageSize,
+      success:(res)=>{
+        console.log(res)
+        this.setData({
+          shopList:res.data
+        })
+      }})
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
